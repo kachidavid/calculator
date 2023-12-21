4faee7aa17b4b58053da3e1706503fa27e464909
kachi <104820825+kachidavid@users.noreply.github.com>
2023-12-20 18:56:35 -0600

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,19 @@
+import math
+
+def add(num1, num2):
+    return num1 + num2
+def subtract(num1, num2):
+    return num1 - num2
+def divide(num1, num2):
+    total = num1 // num2
+    rem = num1 % num2
+    return (total + 'remainder : {rem}')
+def multiply(num1, num2):
+    return num1 * num2
+def square_root(num1):
+    return math.sqrt(num1)
+
+
+
+
+
