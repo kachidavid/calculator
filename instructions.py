3e68ae240d7711fc80249672d7a640c22124e506
kachi <104820825+kachidavid@users.noreply.github.com>
2023-12-20 18:54:52 -0600

    Create instructions.py

---
+++ instructions.py
@@ -0,0 +1,14 @@
+def instruction():
+    print("--------------------------")
+    print("These are the Mathematical Operators Available.")
+    print("Addition: +")
+    print("Subtraction: -")
+    print("Division: //")
+    print("Multiplication: *")
+    print("Square-root: sqrt")
+    print("--------------------------")
+
+
+
+
+#instruction()
